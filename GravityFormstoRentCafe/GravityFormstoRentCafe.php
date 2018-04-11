3add0f60ebd3c80d5c29f528471c1bbf6c1efb9c
alextryonpdx <alextryonpdx@gmail.com>
2018-04-11 14:11:19 -0700

    adding options page

--- GravityFormstoRentCafe/GravityFormstoRentCafe.php
+++ GravityFormstoRentCafe/GravityFormstoRentCafe.php
@@ -16,18 +16,18 @@
 add_action( 'gform_after_submission', 'after_submission_handler' );
 function after_submission_handler( $form ) {
    if( class_exists ('GFAPI')) {
-    $formData = $form;
+       $formData = $form;
 
-    echo $formData[1] . '<br>'; //firstName
-   $leadString = 'https://api.rentcafe.com/rentcafeapi.aspx?requestType=lead&propertyCode=p0647361&username=apileadsuser@greystar.com&password=rentcafe1&addr1=2220%20NW%20Front%20Ave&city=Portland&state=OR&zipCode=97209&source=Website%20contact%20form&' . 'firstName=' . $formData[1] . '&lastName=' . $formData[2] . '&email=' . $formData[3] . '&message=' . urlencode($formData[4]) . '&phone=' . preg_replace("/[^0-9,.]/", "", $formData[5]);
-    echo $formData[2] . '<br>'; //lastName
-    echo $formData[3] . '<br>'; //email
-    echo urlencode($formData[4]) . '<br>'; //message
-    echo preg_replace("/[^0-9,.]/", "", $formData[5]) . '<br>';
-    echo $leadString . '<br>';
-    $info = wp_remote_get( $leadString );//makes GET request
-    print_r($info);
-}
+       echo $formData[1] . '<br>'; //firstName
+      $leadString = 'https://api.rentcafe.com/rentcafeapi.aspx?requestType=lead&propertyCode=p0647361&username=apileadsuser@greystar.com&password=rentcafe1&addr1=2220%20NW%20Front%20Ave&city=Portland&state=OR&zipCode=97209&source=Website%20contact%20form&' . 'firstName=' . $formData[1] . '&lastName=' . $formData[2] . '&email=' . $formData[3] . '&message=' . urlencode($formData[4]) . '&phone=' . preg_replace("/[^0-9,.]/", "", $formData[5]);
+       echo $formData[2] . '<br>'; //lastName
+       echo $formData[3] . '<br>'; //email
+       echo urlencode($formData[4]) . '<br>'; //message
+       echo preg_replace("/[^0-9,.]/", "", $formData[5]) . '<br>';
+       echo $leadString . '<br>';
+       $info = wp_remote_get( $leadString );//makes GET request
+       print_r($info);
+   }
 }
 
 
@@ -36,10 +36,34 @@ function after_submission_handler( $form ) {
 register_activation_hook(__FILE__,'create_default_rentcafe_values');
 
 function create_default_rentcafe_values() {
-   if ( get_option( 'freeze_delay' ) == false ) { 
-            add_option("freeze_delay", $freeze_delay_default); 
-         }
-   }
+   if ( get_option( 'rentcafe_api_url' ) == false ) { 
+            add_option("rentcafe_api_url", $rentcafe_api_url_default); 
+      }
+   if ( get_option( 'rentcafe_property_code' ) == false ) { 
+            add_option("rentcafe_property_code", $rentcafe_property_code_default); 
+      }
+   if ( get_option( 'rentcafe_username' ) == false ) { 
+            add_option("rentcafe_username", $rentcafe_username_default); 
+      }
+   if ( get_option( 'rentcafe_password' ) == false ) { 
+            add_option("rentcafe_password", $rentcafe_password_default); 
+      }
+   if ( get_option( 'rentcafe_lead_source' ) == false ) { 
+            add_option("rentcafe_lead_source", $rentcafe_lead_source_default); 
+      }
+   if ( get_option( 'rentcafe_address' ) == false ) { 
+            add_option("rentcafe_address", $rentcafe_address_default); 
+      }
+   if ( get_option( 'rentcafe_city' ) == false ) { 
+            add_option("rentcafe_city", $rentcafe_city_default); 
+      }
+   if ( get_option( 'rentcafe_state' ) == false ) { 
+            add_option("rentcafe_state", $rentcafe_state_default); 
+      }
+   if ( get_option( 'rentcafe_zip' ) == false ) { 
+            add_option("rentcafe_zip", $rentcafe_zip_default); 
+      }
+}
 
 ////////////////////////////////////////   SETTINGS   ////////////////////////////////////////
 
@@ -77,4 +101,70 @@ function register_rentcafe_settings() {
 // password
 // source description
 // property address (addr1, city, state, zip)
+
+
+function rentcafe_settings_page() {
 ?>
+
+<div class="wrap">
+   <img id="watson-branding" src="<?php echo plugins_url('img/WC_Brand_Signature.png', __FILE__ ); ?>" style="max-width:400px;">
+   <h1>Gravity Forms -> RentCafe Integration</h1>
+   <form method="post" action="options.php"> 
+      <?php 
+      settings_fields( 'rentcafe_option-group' );
+      do_settings_sections( 'rentcafe_option-group' ); ?>
+
+      <table class="form-table rentcafe-options">
+
+        <tr valign="top">
+           <th scope="row">API URL</th>
+           <td><input type="text" step="1" name="rentcafe_api_url" value="<?php echo esc_attr( get_option('rentcafe_api_url') ); ?>" /></td>
+        </tr>
+
+        <tr valign="top">
+           <th scope="row">Username</th>
+           <td><input type="text" step="1" name="rentcafe_username" value="<?php echo esc_attr( get_option('rentcafe_username') ); ?>" /></td>
+        </tr>
+
+        <tr valign="top">
+           <th scope="row">Password</th>
+           <td><input type="text" step="1" name="rentcafe_password" value="<?php echo esc_attr( get_option('rentcafe_password') ); ?>" /></td>
+        </tr>
+
+        <tr valign="top">
+           <th scope="row">Lead Source Tag</th>
+           <td><input type="text" step="1" name="rentcafe_lead_source" value="<?php echo esc_attr( get_option('rentcafe_lead_source') ); ?>" /></td>
+        </tr>
+
+        <tr valign="top">
+           <th scope="row">Property Code</th>
+           <td><input type="text" step="1" name="rentcafe_property_code" value="<?php echo esc_attr( get_option('rentcafe_property_code') ); ?>" /></td>
+        </tr>
+
+        <tr valign="top">
+           <th scope="row">Property City</th>
+           <td><input type="text" step="1" name="rentcafe_city" value="<?php echo esc_attr( get_option('rentcafe_city') ); ?>" /></td>
+        </tr>
+
+        <tr valign="top">
+           <th scope="row">Property State</th>
+           <td><input type="text" step="1" name="rentcafe_state" value="<?php echo esc_attr( get_option('rentcafe_state') ); ?>" /></td>
+        </tr>
+
+        <tr valign="top">
+           <th scope="row">Property Zipcode</th>
+           <td><input type="text" step="1" name="rentcafe_zip" value="<?php echo esc_attr( get_option('rentcafe_zip') ); ?>" /></td>
+        </tr>
+
+    </table>
+
+    <?php
+      submit_button('Save Changes and Reset Timer');
+      ?>
+   </form>
+
+   <?php 
+}
+
+
+   ?>
